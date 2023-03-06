2bd64902ee19df769b6c58bfac1f34c57c9c1733
Adrien.Courtois <adrien.courtois@outlook.com>
2023-03-06 23:23:21 +0100

    update run.sh add mkdir profile

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+[ ! -d profile ] && mkdir profile
+
 HOST_DISPLAY=1 docker run -it --env=DISPLAY -v `pwd`/projets:/home/android/projets -v `pwd`/profile:/home/android/Android -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add plugdev studio bash
